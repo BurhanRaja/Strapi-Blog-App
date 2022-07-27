b56d3c5509af8072fe211ef341fc94a65d85c1b5
BurhanuddinRaja <mails4burhan@gmail.com>
2022-07-27 11:24:47 +0530

    Text Editor added and category, post added

--- src/api/category/services/category.js
+++ src/api/category/services/category.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * about service.
+ * category service.
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::about.about');
+module.exports = createCoreService('api::category.category');
